3c62aeca57a610ea5557e6f5c73b3966b32cba00
Kaan Denizay <kaan.denizay.ext@ozan.com>
2025-07-20 19:20:48 +0300

    Report improvement and parameter choose implemented

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty",
                 "json:target/cucumber.json",
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "classpath:features",
         glue = "stepDefinitions",
         dryRun =false,
-        tags ="@regression"
+        tags ="@regression",
+        monochrome = true
 
 
 )
